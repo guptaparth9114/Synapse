53c0b0c7e24d7268eb799504ca6313692c5c5eeb
Prakriti Yadav <prakritiyadav2004@gmail.com>
2024-03-23 18:50:32 +0530

    hero2

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,50 +1,6 @@
-// import React from 'react'
-// import r from "../components/r.jpg";
-
-// const Hero = () => {
-//   return (
-//     <div >
-//       <div className="w-screen h-screen flex items-center justify-center">
-//           <div className="absolute inset-0 bg-black opacity-85"></div>
-
-//       <img src={r} alt="Background" className="object-cover w-full h-full " />
-//       <div className=" flex flex-col absolute inset-0 flex items-center justify-center text-white text-3xl font-semibold m-2 p-4 ">
-//       <h1 className="text-5xl mb-4">Your Wellness, Our Priority</h1>
-//       <div className="flex flex-col items-center justify-center">
-//       <button className=" w-[200%] flex flex-col justify-center items-center rounded-full shadow-lg bg-gradient-to-r from-black to-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 border-4 border-white">
-//         <span className="text-s">Login / Sign up</span>
-//         <span className="text-xs">For Patient as well as Doctor</span>
-//       </button>
-//       <button className="w-[200%] flex flex-col justify-center items-center rounded-full shadow-lg bg-gradient-to-r from-black to-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 border-4 border-white">
-//         <span className="text-s">Find your Doctor</span>
-//       </button>
-
-//       <button className="w-[200%] flex flex-col justify-center items-center rounded-full shadow-lg bg-gradient-to-r from-black to-gray-600 m-2 p-4 cursor-pointer  hover:scale-110  ease-in duration-200 border-4 border-white">
-//         <span className="text-s">Print Your Prescriptions</span>
-//       </button>
-
-//       <button className="w-[200%] flex flex-col justify-center items-center rounded-full shadow-lg bg-gradient-to-r from-black to-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 border-4 border-white">
-//         <span className="text-s">My Calender</span>
-//       </button>
-
-
-//     </div>
-
-    
-//       </div>
-      
-      
-//     </div>
-//     </div>
-//   )
-// }
-
-// export default Hero
-
-
 
 import React from 'react';
-import l from "../components/l.jpg";
+import r from "../components/r.jpg";
 
 const Hero = () => {
   const gradientBorder = {
@@ -58,7 +14,7 @@ const Hero = () => {
     <div>
       <div className="w-screen h-screen flex items-center justify-center relative">
         <div className="absolute inset-0 bg-black opacity-70"></div>
-        <img src={l} alt="Background" className="object-cover w-full h-full" />
+        <img src={r} alt="Background" className="object-cover w-full h-full" />
         <div className="absolute inset-0 flex items-center justify-center text-white text-3xl  font-semibold m-2 p-4 flex-col">
           <div className="text-white-800 font-mono mb-8 border-4 "  style={gradientBorder} >
             <h1 className="text-4xl mb-8 ">
