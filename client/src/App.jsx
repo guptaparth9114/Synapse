24215dc3d9eede530a972fc723d19ad1a00639c1
ShivPandey5677 <pandeyshiv525@gmail.com>
2024-03-23 15:01:56 +0530

    Added Register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,62 @@
-import { useState } from 'react'
+import Navbar from "./components/Navbar";
+
+import Login from "./pages/login";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Register from "./pages/register";
+import Home from "./pages/Home";
+
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { useContext } from "react";
+// import { AuthContext } from "./context/authContext";
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const {currentUser}=useContext(AuthContext);
+  // const queryClient=new QueryClient();
+  const Layout=()=>{
+    return (
+      // <QueryClientProvider client={queryClient}>
+       <div className="bg-[#86B6F6]">
+      <Navbar/>
+        <Outlet/>
+        </div>
+      //  </QueryClientProvider>
+    );
 
-  return (
-    <>
-    <h1>HII</h1>
+  };
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:
+      <Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
        
-    </>
-  )
-}
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+   
+  ]);
+ 
+    return <div >
+       <RouterProvider router={router} />
+    </div>;
+  } 
 
 export default App
