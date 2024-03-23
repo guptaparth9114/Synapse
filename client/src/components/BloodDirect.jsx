0b3359e90a3758716d5f5f7b1bdaa174a2105360
Prakriti Yadav <prakritiyadav2004@gmail.com>
2024-03-23 23:33:16 +0530

    blooddirect

--- client/src/components/BloodDirect.jsx
+++ client/src/components/BloodDirect.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+
+
+import React from 'react';
 
 const BloodDirect = () => {
   return (
-    <div>
-      <div className="w-screen h-[80vh] flex items-center justify-center relative">
-        <div className="absolute inset-0 bg-black opacity-70"></div>
-        <img src="assets/bb.jpg" alt="Background" className="object-cover w-full h-full" />
+    <div className="relative w-screen h-screen">
+      {/* Background image */}
+      <img src="assets/bl.jpg" alt="Background" className="absolute inset-0 object-cover w-full h-full" />
+
+      {/* Overlay */}
+      <div className="absolute inset-0 flex flex-col items-center justify-center bg-black opacity-70  ">
+        {/* Quotation */}
+        <h1 className="text-white text-3xl font-bold mb-8 "><em>"The blood you donate gives someone another chance at life. One day that someone may be a close relative, a friend, a loved one—or even you."</em></h1>
+
+        {/* Button */}
+        <button className="bg-white text-black px-10 py-4 rounded-md shadow-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 border-4 border-yellow ">
+         <h2 className="font-bold"> Visit Blood Bank portal </h2>
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BloodDirect
+export default BloodDirect;
+
